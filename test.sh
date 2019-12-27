f0f00cbe1cc343ec6e82431a49f976eb8e1664c9
kimbo <kimballleavitt@gmail.com>
2019-12-27 15:24:28 -0700

    added tests

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+doh_bin="./bin/doh.js"
+doh_test() {
+	output=$("$doh_bin" $@ 2>&1)
+	exit_code="$?"
+	if [ "$exit_code" -eq 0 ]; then
+		echo "Command \"$doh_bin $@\" PASSED"
+	else
+		echo -e "Command \"$doh_bin $@\" FAILED"
+		echo -e "OUTPUT:\n$output" >&2
+		exit "$exit_code"
+	fi
+}
+doh_test -h
+doh_test --help
+doh_test --version
+doh_test https://dns.google/dns-query -m GET
+doh_test https://dns.google/dns-query
+doh_test https://cloudflare-dns.com/dns-query -t AAAA
+doh_test https://cloudflare-dns.com/dns-query -q example.net -t TXT -m GET
+doh_test https://dns.quad9.net/dns-query
+doh_test https://dns.quad9.net/dns-query -m GET -q com -t NS
