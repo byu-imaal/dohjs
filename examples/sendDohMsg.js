334fd6fbdc5a6717bda4974788311f663b6fb93c
kimbo <kimballleavitt@gmail.com>
2020-02-17 22:44:17 -0700

    make examples easier to copy/paste into browser code

--- examples/sendDohMsg.js
+++ examples/sendDohMsg.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const {makeQuery, sendDohMsg} = require('dohjs');
+const doh = require('dohjs');
 
 const url = 'https://cloudflare-dns.com/dns-query';
 const method = 'GET';
 
 // create a query message
-let msg = makeQuery('example.com', 'TXT');
+let msg = doh.makeQuery('example.com', 'TXT');
 
 // send it and print out the response to the console
-sendDohMsg(msg, url, method)
+doh.sendDohMsg(msg, url, method)
   .then(response => response.answers.forEach(ans => console.log(ans.data.toString())))
   .catch(console.error);
