419ecd81bed9d2aff950ec6c74039359e2aad626
jacobgb24 <jacobldavis98@gmail.com>
2020-02-20 23:19:25 -0700

    removed redundant ready call

--- main.js
+++ main.js
@@ -11,14 +11,23 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
     // SET UP
     // enable popovers
-    $(document).ready(function($){
-        $('[data-toggle="popover"]').popover();
-    });
+    $('[data-toggle="popover"]').popover();
     // ignore clicks on popovers to make clicking links easier
     $('body').on('mousedown', '.popover', function(e) {
         e.preventDefault()
     });
 
+    const forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
 
     // FUNCTIONS
     const errorFunction = (err) => {
