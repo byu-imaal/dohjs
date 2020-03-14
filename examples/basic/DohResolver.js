713b0c0d1e1021f1eaaf324085ea94ffcd84780d
kimbo <kimballleavitt@gmail.com>
2020-03-14 15:57:24 -0600

    added example with timeout

--- examples/basic/DohResolver.js
+++ examples/basic/DohResolver.js
@@ -11,3 +11,18 @@ resolver.query('example.com', 'A')
     response.answers.forEach(ans => console.log(ans.data));
   })
   .catch(err => console.error(err));
+
+
+// Now we'll do the same query, but this time,
+// it's going to be a GET request, and
+// we're going to set a timeout for one second
+
+// timeout in milliseconds
+const timeout = 1000;
+
+// lookup the A records for example.com
+resolver.query('example.com', 'A', 'GET', null, timeout)
+  .then(response => {
+    response.answers.forEach(ans => console.log(ans.data));
+  })
+  .catch(err => console.error(err));
